6cd672f45d2004ffa40561864d7d51aa08016e51
Vijay Singh <dprophecyguy@gmail.com>
2023-08-17 20:58:13 +0530

    💻 Virtual list component working

--- React/component-practice/src/pages/Virtaul-List/VirtualList.jsx
+++ React/component-practice/src/pages/Virtaul-List/VirtualList.jsx
@@ -1,66 +1,72 @@
-// remove PropType warning eslint
-/* eslint-disable react/prop-types */
 import { useEffect, useRef, useState } from "react";
 import "./styles.css";
 
-function VirtualListItem({ item }) {
-  return <div className="list-item">{item.title}</div>;
-}
-
-function VirtualList({ list, rowHeight }) {
-  const VIEWPORT_HEIGHT = 400;
-  const BUFFER = 10;
-
-  const visibleItemsCount = Math.ceil(VIEWPORT_HEIGHT / rowHeight) + 2 * BUFFER;
-  console.log(visibleItemsCount);
-
+const VirtualList = ({ items, itemHeight, renderRow }) => {
+  const containerRef = useRef();
   const [startIndex, setStartIndex] = useState(0);
-  const containerRef = useRef(null);
+  const [visibleCount, setVisibleCount] = useState(0);
 
-  const handleScroll = () => {
-    const scrollPosition = containerRef.current.scrollTop;
-    console.log({ scrollPosition });
-    const newStart = Math.max(
-      0,
-      Math.floor(scrollPosition / rowHeight) - BUFFER
-    );
-    setStartIndex(Math.max(0, newStart));
+  const updateStartIndex = () => {
+    const container = containerRef.current;
+    const scrollPosition = container.scrollTop;
+    setStartIndex(Math.max(0, Math.floor(scrollPosition / itemHeight)));
   };
 
-  // Scroll event listener
   useEffect(() => {
-    const conatainer = containerRef.current;
-    if (!conatainer) return;
+    const container = containerRef.current;
+    setVisibleCount(Math.ceil(container.clientHeight / itemHeight) + 2); // 2 extra for buffer
+    container.addEventListener("scroll", updateStartIndex);
+    return () => container.removeEventListener("scroll", updateStartIndex);
+  }, [itemHeight]);
 
-    conatainer.addEventListener("scroll", handleScroll);
-    return () => {
-      conatainer.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  // Calculate the visible window
-  const end = Math.min(startIndex + visibleItemsCount, list.length);
-  console.log({ startIndex, end, list: list.length });
-  const visibleItems = list.slice(startIndex, end);
+  const visibleItems = [];
+  for (
+    let i = startIndex;
+    i < startIndex + visibleCount && i < items.length;
+    i++
+  ) {
+    visibleItems.push(
+      <div
+        key={i}
+        style={{
+          position: "absolute",
+          top: `${i * itemHeight}px`,
+          height: `${itemHeight}px`,
+        }}
+      >
+        {renderRow(items[i], i)}
+      </div>
+    );
+  }
 
   return (
-    <div className="virtual-list-container" ref={containerRef}>
+    <div
+      ref={containerRef}
+      style={{ height: "300px", overflowY: "auto", position: "relative" }}
+    >
       <div
-        style={{ height: `${list.length * rowHeight}px`, position: "relative" }}
+        style={{
+          height: `${items.length * itemHeight}px`,
+          width: "80px",
+          position: "relative",
+        }}
       >
-        {visibleItems.map((item, index) => (
-          <VirtualListItem key={index} item={item} />
-        ))}
+        {visibleItems}
       </div>
     </div>
   );
-}
+};
+
+export default VirtualList;
+
+const renderItem = (item, index) => (
+  <div key={index} style={{ height: "30px" }}>
+    {item}
+  </div>
+);
 
 export function DemoVirtualList() {
-  const list = Array.from({ length: 1000 }).map((_, i) => ({
-    id: i,
-    title: `Item ${i}`,
-  }));
+  const items = Array.from({ length: 10000 }, (_, i) => `Item ${i}`);
 
-  return <VirtualList list={list} rowHeight={30} />;
+  return <VirtualList items={items} itemHeight={30} renderRow={renderItem} />;
 }
